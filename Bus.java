6091cc22dd8339520e01bf7f077f7ebeec2f24d0
Volha_Ivanova <Volha_Ivanova@10.6.211.53>
2017-01-19 15:41:26 +0300

    change file

--- Bus.java
+++ Bus.java
@@ -6,6 +6,7 @@ String station;
 
 	public void drive(String station) {
 		System.out.println("Автобус с номером "+number+ " отъехал от остановки "+ station);		
+		//System.out.println("Автобус с номером "+number+ " отъехал от остановки "+ station);	
 	}
 	public void stop(String station) {
 		System.out.println("Автобус с номером "+number+ " остановился на остановке "+station);
