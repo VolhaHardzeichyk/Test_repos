dd97560687fe776665c1a02ad6feca5eb854c36c
Volha Ivanova <Volha_Ivanova@EPBYMINW4219.local>
2017-01-19 15:18:19 +0300

    new file

---
+++ Bus.java
@@ -0,0 +1,14 @@
+package Hometask.lesson1;
+
+public class Bus {
+int number;
+String station;
+
+	public void drive(String station) {
+		System.out.println("Автобус с номером "+number+ " отъехал от остановки "+ station);		
+	}
+	public void stop(String station) {
+		System.out.println("Автобус с номером "+number+ " остановился на остановке "+station);
+	}
+
+}
